71c5793ef11ce8d79ef08efbd07549a076690c5e
HeBa-15 <hebamahmoudali1510@gmail.com>
2025-01-20 00:21:05 +0200

    Interface EXample 01

---
+++ OOP04/Interface Example01/TypeA.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP04.Interface_Example01
+{
+    internal class TypeA : IType
+    {
+        public int MyProperty { get; set; }
+        public void MyMethod()
+        {
+            Console.WriteLine("Hello World!"); ;
+        }
+    }
+}
--- OOP04/Program.cs
+++ OOP04/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // IType reference;
+            // Declare for reference of type "IType", containing garbage value
+            /// this Reference can refer to an Object of any type Implementing IType Interface
+            /// CLR will Allocate 4 unintialized Bytes in Stack for this reference
+            /// CLR will allocate 0 Bytes in HEAP
+            
+            // reference = new IType(); INVALID
+            //reference = new TypeA();
+            //reference.MyProperty = 10;
+            //reference.MyMethod();
+            //reference.Print();
+            //TypeA typeA = new TypeA();
+            //typeA.MyProperty = 20;
+            //typeA.MyMethod();
+            //typeA.Print():   // INVALID , can't see this method
+
         }
     }
 }
