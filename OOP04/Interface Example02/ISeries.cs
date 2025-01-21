f4480fd1f2de495fd7f501208fe26d18c3bd1d63
HeBa-15 <hebamahmoudali1510@gmail.com>
2025-01-21 03:05:24 +0200

    Interface Example 02

---
+++ OOP04/Interface Example02/ISeries.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP04.Interface_Example02
+{
+    internal interface ISeries
+    {
+
+        public int Current { get; set; }
+        public void GetNext();
+        public void Reset()
+        {
+            Current = 0;
+        }
+    }
+}
--- OOP04/Program.cs
+++ OOP04/Program.cs
@@ -1,15 +1,35 @@
-﻿namespace OOP04
+﻿using OOP04.Interface_Example02;
+
+namespace OOP04
 {
     internal class Program
     {
+
+        #region Interface Ex02
+        static void PrintTenNUmbersFromSeries(ISeries series)
+        {
+            if (series == null)
+            {
+                return;
+            }
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.Write($"{series.Current} \t");
+                series.GetNext();
+            }
+            series.Reset();
+            Console.WriteLine();
+        }
+        #endregion
         static void Main(string[] args)
         {
+            #region Interface EXample 01
             // IType reference;
             // Declare for reference of type "IType", containing garbage value
             /// this Reference can refer to an Object of any type Implementing IType Interface
             /// CLR will Allocate 4 unintialized Bytes in Stack for this reference
             /// CLR will allocate 0 Bytes in HEAP
-            
+
             // reference = new IType(); INVALID
             //reference = new TypeA();
             //reference.MyProperty = 10;
@@ -19,6 +39,23 @@
             //typeA.MyProperty = 20;
             //typeA.MyMethod();
             //typeA.Print():   // INVALID , can't see this method
+
+            #endregion
+
+
+
+            #region Interface Example 02
+
+            //TypeA seriesByTwo = new TypeA();
+            //PrintTenNUmbersFromSeries(seriesByTwo);
+
+            //TypeB seriesByThree = new TypeB();
+            //PrintTenNUmbersFromSeries(seriesByThree);
+
+            //TypeC seriesByFour = new TypeC();
+            //PrintTenNUmbersFromSeries(seriesByThree);
+            //
+            #endregion
 
         }
     }
